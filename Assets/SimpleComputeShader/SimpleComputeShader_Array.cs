43d2c4ab70e8af7787137ad511097a0dbc816e7e
h_nishihata <nishihata0301@gmail.com>
2021-11-06 23:19:47 +0900

    add SimpleComputeBuffer

---
+++ Assets/SimpleComputeShader/SimpleComputeShader_Array.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class SimpleComputeShader_Array : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    int kernelIndex_KernelFunction_A;
+    int kernelIndex_KernelFunction_B;
+
+    ComputeBuffer intComputeBuffer;
+    void Start()
+    {
+        this.kernelIndex_KernelFunction_A = this.computeShader.FindKernel("KernelFunction_A");
+        this.kernelIndex_KernelFunction_B = this.computeShader.FindKernel("KernelFunction_B");
+
+        this.intComputeBuffer = new ComputeBuffer(4, sizeof(int));
+        this.computeShader.SetBuffer
+            (this.kernelIndex_KernelFunction_A, "intBuffer", this.intComputeBuffer);
+
+        this.computeShader.SetInt("intValue", 3);
+
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_A, 1, 1, 1);
+
+        int[] result = new int[4];
+        this.intComputeBuffer.GetData(result);
+        Debug.Log("RESULT: KenelFunction_A");
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.Log(result[i]);
+        }
+
+
+        this.computeShader.SetBuffer
+            (this.kernelIndex_KernelFunction_B, "intBuffer", this.intComputeBuffer);
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_B, 1, 1, 1);
+
+        this.intComputeBuffer.GetData(result);
+        Debug.Log("RESULT: KenelFunction_B");
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.Log(result[i]);
+        }
+
+        this.intComputeBuffer.Release();
+    }
+}
---
+++ Assets/SimpleComputeShader/SimpleComputeShader_Texture.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class SimpleComputeShader_Texture : MonoBehaviour
+{
+    public GameObject plane_A;
+    public GameObject plane_B;
+
+    public ComputeShader computeShader;
+
+    RenderTexture renderTexture_A;
+    RenderTexture renderTexture_B;
+
+    int kernelIndex_KernelFunction_A;
+    int kernelIndex_KernelFunction_B;
+
+    struct ThreadSize
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public ThreadSize(uint x, uint y, uint z)
+        {
+            this.x = (int)x;
+            this.y = (int)y;
+            this.z = (int)z;
+        }
+    }
+
+    ThreadSize kernelThreadSize_KernelFunction_A;
+    ThreadSize kernelThreadSize_KernelFunction_B;
+    void Start()
+    {
+        this.renderTexture_A = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB32);
+        this.renderTexture_B = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB32);
+
+        this.renderTexture_A.enableRandomWrite = true;
+        this.renderTexture_B.enableRandomWrite = true;
+
+        this.renderTexture_A.Create();
+        this.renderTexture_B.Create();
+
+        this.kernelIndex_KernelFunction_A = this.computeShader.FindKernel("KernelFunction_A");
+        this.kernelIndex_KernelFunction_B = this.computeShader.FindKernel("KernelFunction_B");
+
+        uint threadSizeX, threadSizeY, threadSizeZ;
+
+        this.computeShader.GetKernelThreadGroupSizes
+            (this.kernelIndex_KernelFunction_A,
+            out threadSizeX, out threadSizeY, out threadSizeZ);
+
+        this.kernelThreadSize_KernelFunction_A
+            = new ThreadSize(threadSizeX, threadSizeY, threadSizeZ);
+
+        this.computeShader.GetKernelThreadGroupSizes
+            (this.kernelIndex_KernelFunction_B,
+            out threadSizeX, out threadSizeY, out threadSizeZ);
+
+        this.kernelThreadSize_KernelFunction_B
+            = new ThreadSize(threadSizeX, threadSizeY, threadSizeZ);
+
+        this.computeShader.SetTexture
+            (this.kernelIndex_KernelFunction_A, "textureBuffer", this.renderTexture_A);
+        this.computeShader.SetTexture
+            (this.kernelIndex_KernelFunction_B, "textureBuffer", this.renderTexture_B);
+    }
+
+    private void Update()
+    {
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_A,
+                                    this.renderTexture_A.width / this.kernelThreadSize_KernelFunction_A.x,
+                                    this.renderTexture_A.height / this.kernelThreadSize_KernelFunction_A.y,
+                                    this.kernelThreadSize_KernelFunction_A.z);
+
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_B,
+                                    this.renderTexture_B.width / this.kernelThreadSize_KernelFunction_B.x,
+                                    this.renderTexture_B.height / this.kernelThreadSize_KernelFunction_B.y,
+                                    this.kernelThreadSize_KernelFunction_B.z);
+
+        plane_A.GetComponent<Renderer>().material.mainTexture = this.renderTexture_A;
+        plane_B.GetComponent<Renderer>().material.mainTexture = this.renderTexture_B;
+    }
+}
